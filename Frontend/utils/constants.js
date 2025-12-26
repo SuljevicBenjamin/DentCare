f04c113cd2963b74fd654e7417548d6b6a95b34b
SuljevicBenjamin <benjamin.suljevic@stu.ibu.edu.ba>
2025-12-26 18:15:59 +0100

    Milestone5

--- Frontend/utils/constants.js
+++ Frontend/utils/constants.js
@@ -1,13 +1,6 @@
 let Constants = {
-  PROJECT_BASE_URL: "https://dentcare-api.ondigitalocean.app/",
-  USER_ROLE: "user",
-  ADMIN_ROLE: "admin"
-};
-
-
-//let Constants = {
-//    PROJECT_BASE_URL: "https://dentcare-m3m3o.ondigitalocean.app/Backend/",
-//    USER_ROLE: "user",
-//    ADMIN_ROLE: "admin"
-// }
+    PROJECT_BASE_URL: "https://dentcare-m3m3o.ondigitalocean.app/Backend/",
+    USER_ROLE: "user",
+    ADMIN_ROLE: "admin"
+ }
  
